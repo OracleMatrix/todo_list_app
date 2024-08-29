61f4f22bc18051d48a16ed042ade12128a523aac
Ehsan <ehsanmohamadipoor@gmail.com>
2024-08-30 01:19:58 +0330

    updated to 1.1.0

--- lib/edit.screen.dart
+++ lib/edit.screen.dart
@@ -46,7 +46,7 @@ class _EditTaskScreenState extends State<EditTaskScreen> {
                       fontSizeFactor: 1.2,
                     ),
               ),
-            )
+            ),
           ],
         ),
       ),
