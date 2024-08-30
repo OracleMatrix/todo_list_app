09702db4de82b0da80df83b1fd664f9995e3a0f9
Ehsan <ehsanmohamadipoor@gmail.com>
2024-08-30 14:12:42 +0330

    ToDo main files

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:todo_list/pages/home_screen.dart';
 
 const taskBoxName = "tasks";
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   Hive.registerAdapter(TaskEntityAdapter());
   Hive.registerAdapter(PriorityAdapter());
